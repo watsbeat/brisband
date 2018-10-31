85797579a7dafe4d267192836bd53f31dc42461f
watsbeat <c.watsonbeaton@gmail.com>
2018-10-31 21:16:39 +1000

    created seeds for bands & user; updated seed.rb to destroy records first when db:seed command is run (for blank slate when adding more seeds, so old records aren't added again); modified seed.rb to print message re number of records (items, users, & bands) created when seed command is run

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Keep all the record creation needed to seed the database with its default values
+# Run db:seed command when necessary
+
+# Destroy all records to have a blank slate - so we can run seed command as many times as we want.
+Item.destroy_all
+User.destroy_all
+Band.destroy_all
+
+# Create seeds for items, users and bands.
 items = Item.create([
     {
         brand: "Ibanez",
@@ -38,3 +40,79 @@ items = Item.create([
         price: 0.00
     }
 ])
+
+users = User.create([
+    {
+        email: "bruce@gmail.com",
+        password: "bruceblue",
+        name: "Bruce Blue",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
+        genres: "Rock, Punk" 
+    }, 
+    {
+        email: "mary@gmail.com",
+        password: "marymauve",
+        name: "Mary Mauve",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
+        genres: "Indie, Pop" 
+    },
+    {
+        email: "lars@gmail.com",
+        password: "larslavender",
+        name: "Lars Lavender",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
+        genres: "Heavy Metal" 
+    },
+    {
+        email: "georgia@gmail.com",
+        password: "georgiagreen",
+        name: "Georgia Green",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
+        genres: "Folk" 
+    },
+    {
+        email: "owen@gmail.com",
+        password: "owenorange",
+        name: "Owen Orange",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
+        genres: "Blues, Alternative" 
+    }
+])
+
+bands = Band.create([
+    {
+        name: "Apple Band",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
+        genre: "Punk",
+        status: true       
+    },
+    {
+        name: "Blueberry Band",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
+        genre: "Blues, Alternative",
+        status: true       
+    },
+    {
+        name: "Banana Band",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
+        genre: "Rock",
+        status: false       
+    },
+    {
+        name: "Strawberry Band",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
+        genre: "Indie, Pop",
+        status: true       
+    },
+    {
+        name: "Mango Band",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
+        genre: "Heavy Metal",
+        status: false      
+    }
+]) 
+
+# Prints number of items, users & bands created
+p "Created #{Item.count} items"
+p "Created #{User.count} users"
+p "Created #{Band.count} bands"
