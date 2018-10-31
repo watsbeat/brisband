72631845438fad32fc800c315a326b1706e4a9e5
thehicksivist <hicks.a.james@gmail.com>
2018-10-31 10:44:41 +1000

    samples seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,32 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 items = Item.create([
     {
-        make: "Ibanez",
+        brand: "Ibanez",
         model: "RGA320",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
-        price: 
+        price: 0.00
         
     }, 
     {
-        make: "Pearl",
+        brand: "Pearl",
         model: "Demon Drive Pedals",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
         price: 0.00
     },
     {
-        make: "Pearl",
+        brand: "Pearl",
         model: "EXPORT EXL",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
         price: 1000.00
     },
     {
-        make: "Shure",
+        brand: "Shure",
         model: "SM-58",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
         price: 0.00
     },
     {
-        make: "Warwick",
+        brand: "Warwick",
         model: "Thumb NT 1988",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dapibus elit. Aenean sed nunc volutpat, vulputate metus sed, interdum nisi. Morbi efficitur, augue et tincidunt iaculis, ligula lorem pellentesque metus, consequat euismod lectus tellus in neque. Maecenas fringilla efficitur mi ac porta. Vestibulum id orci tortor. Fusce cursus, leo.",
         price: 0.00
