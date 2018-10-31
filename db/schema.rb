8de11f3d0f10cb07af1bbb728e56c3ee13a8b90a
watsbeat <c.watsonbeaton@gmail.com>
2018-10-31 14:10:05 +1000

    ran database migrate for comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_062517) do
+ActiveRecord::Schema.define(version: 2018_10_31_035730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "brand"
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_10_30_062517) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,7 +40,6 @@ ActiveRecord::Schema.define(version: 2018_10_30_062517) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
