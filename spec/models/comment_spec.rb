6e020d4d0193b4dfaca3471567f7bff92b5fe565
watsbeat <c.watsonbeaton@gmail.com>
2018-11-02 16:36:07 +1000

    test that comments can be created when given correct parameters

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+# Comments can be created when given the correct parameters
+  it "creates new comments with correct fields" do
+    comment = Comment.create(description:"FooDescription")
+    expect(comment.description).to eq("FooDescription")
+  end
+
 end
