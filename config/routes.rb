d7b04e507136378d159a8fb52d475834011cc9f4
thehicksivist <hicks.a.james@gmail.com>
2018-11-04 20:03:16 +1000

    added guidelines page, route and link to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :bands, :comments, :items, :charges
   get 'user/:id', to: 'user#show', as: 'user'
   devise_for :users
+  get 'home/guidelines'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
