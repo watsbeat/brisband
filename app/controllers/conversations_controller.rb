857604b0caea6af469815d4345d4ba4f19f19553
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-11-04 21:35:13 +1000

    added views for inbox, sent, trash and all, and a notification for unread messages on topnav

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,13 +1,32 @@
 class ConversationsController < ApplicationController
+    before_action :set_conversation, only: [:show, :destroy]
+
     def index
         @conversations = current_user.mailbox.conversations
     end
 
+    def inbox
+        @conversations = current_user.mailbox.inbox
+        render action: :index
+    end
+
+    def sent
+        @conversations = current_user.mailbox.sentbox
+        render action: :index
+    end
+
+    def trash
+        @conversations = current_user.mailbox.trash
+        render action: :index
+    end
+
     def show
-        @conversation = current_user.mailbox.conversations.find(params[:id])
+        @conversation.mark_as_read(current_user)
+        @message = Mailboxer::Message.new
     end
 
     def new
+        @conversation = Mailboxer::Conversation.new
         @recipients = User.all - [current_user]
     end
 
@@ -15,5 +34,19 @@ class ConversationsController < ApplicationController
         recipient = User.find(params[:user_id])
         receipt = current_user.send_message(recipient, params[:body], params[:subject])
         redirect_to conversation_path(receipt.conversation)
+    end
+
+    def destroy
+        @conversation.move_to_trash(current_user)
+        respond_to do |format|
+        format.html { redirect_to conversations_url, notice: 'Conversation was successfully destroyed.' }
+        format.json { head :no_content }
+        end
+    end
+
+    private
+
+    def set_conversation
+        @conversation = current_user.mailbox.conversations.find(params[:id])
     end
 end
