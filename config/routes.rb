898338d6a3863da77c02de0e06deb592109e1a03
Crystal-JMK <Crystaljknight94@gmail.com>
2018-11-05 10:23:06 +1000

    Admin page installed and working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :bands, :comments, :items, :charges, :messages
   
   resources :conversations do
